4224108e4dfd13f93df559a760c2953e3ee2ac16
XQF4K <8header8@googlemail.com>
2017-04-05 18:53:52 +0200

    test

---
+++ Test Projekt/src/Starter/Starter.java
@@ -0,0 +1,19 @@
+package Starter;
+/*
+ Programm von Fabi, Sven, Buesra, Gaetano, Merve
+ V 0.0001
+ */
+import GUI.GUI;
+import XML.XML;
+public class Starter {
+	public static void main(String[] args) 
+	{
+		
+		GUI gui=new GUI();
+		XML xml= new XML();
+		xml.toXML();
+		xml.xmlheader+=xml.xmltext;
+		xml.makexml(xml.xmlheader);
+		System.out.println(xml.xmltext);			
+	}
+}
